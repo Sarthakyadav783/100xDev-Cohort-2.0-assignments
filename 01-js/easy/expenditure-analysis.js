3abdd0163b6b0e52f10d439335ff793cf0690c57
Sarthakyadav783 <136995087+Sarthakyadav783@users.noreply.github.com>
2023-12-31 16:13:36 +0530

    Update expenditure-analysis.js

--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -14,7 +14,56 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  const categoryMap = {};
+
+  // Iterate through each transaction using a for loop
+  for (let i = 0; i < transactions.length; i++) {
+    const transaction = transactions[i];
+    const { category, price } = transaction;
+
+    // If category already exists in the map, add the price to the total
+    if (categoryMap[category]) {
+      categoryMap[category].totalSpent += price;
+    } else {
+      // If category doesn't exist, create a new entry in the map
+      categoryMap[category] = { category, totalSpent: price };
+    }
+  }
+    console.log(categoryMap);
+  // Convert the map values to an array of objects
+  const result = Object.values(categoryMap);
+
+  return result;
 }
 
+// Example usage:
+const transactions = [
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  },
+  {
+    id: 2,
+    timestamp: 1656176800000,
+    price: 20,
+    category: 'Food',
+    itemName: 'Burger',
+  },
+  {
+    id: 3,
+    timestamp: 1656276800000,
+    price: 15,
+    category: 'Electronics',
+    itemName: 'Headphones',
+  },
+];
+
+const result = calculateTotalSpentByCategory(transactions);
+console.log(result);
+
+
 module.exports = calculateTotalSpentByCategory;
+
