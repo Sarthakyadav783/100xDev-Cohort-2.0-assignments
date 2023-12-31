9642e0ac136ab1866e84ea251a64ef4c73ce485c
Sarthakyadav783 <136995087+Sarthakyadav783@users.noreply.github.com>
2023-12-31 16:13:05 +0530

    Update anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,28 @@
 */
 
 function isAnagram(str1, str2) {
+  // Remove spaces and convert to lowercase for case-insensitive comparison
+  const cleanStr1 = str1.replace(/\s/g, '').toLowerCase();
+  const cleanStr2 = str2.replace(/\s/g, '').toLowerCase();
 
+  // Check if the lengths are different, if so, they can't be anagrams
+  if (cleanStr1.length !== cleanStr2.length) {
+    return false;
+  }
+
+  // Sort the characters in both strings and compare
+  const sortedStr1 = cleanStr1.split('').sort().join('');
+  const sortedStr2 = cleanStr2.split('').sort().join('');
+
+  return sortedStr1 === sortedStr2;
 }
+
+// Example usage:
+const result1 = isAnagram('listen', 'silent'); // true
+const result2 = isAnagram('hello', 'world');   // false
+
+console.log(result1);
+console.log(result2);
+
 
 module.exports = isAnagram;
